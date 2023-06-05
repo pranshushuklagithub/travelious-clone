5ece2204d86bd9f8519147a1f5e023b573ce46a6
pranshushuklagithub <pranshushuklaoffice@gmail.com>
2023-06-05 14:47:19 +0530

    final prod

--- travelious-clone/src/Components/ProductPage/Accordian.jsx
+++ travelious-clone/src/Components/ProductPage/Accordian.jsx
@@ -9,6 +9,13 @@ export const Accordian=({img})=>{
         inclusion:false,
         thingToCarry:false
     })
+    const images={
+      img1:"https://media.istockphoto.com/id/1422162778/photo/stylish-room-interior-with-sleeper-sofa-near-white-wall-additional-place-for-guest.webp?b=1&s=170667a&w=0&k=20&c=3mswQ57tUyw16lIWGpYZ3W7mqmT9Lr1eoxEDKrfcucI=",
+      img2:"https://media.istockphoto.com/id/1262278932/photo/ready-healthy-food-catering-menu-in-lunch-boxes-fish-and-vegetable-packages-as-daily-meal.webp?b=1&s=170667a&w=0&k=20&c=5cgdTB4TCKHx_nhhz-d704K7H6Ja68kTcZCH8GOZ7J4=",
+      img3:"https://media.istockphoto.com/id/113232749/photo/overhead-of-young-girl-in-orange-life-preserver.webp?b=1&s=170667a&w=0&k=20&c=ttD2E6o0crKWxHzlxmkpSQJULWAwb1_tBqqhCCtxVWQ=",
+      img4:"https://media.istockphoto.com/id/1161578650/photo/unrecognizable-small-passenger-van-hurry-up-on-highway-at-city-street-traffic-with-urban.webp?b=1&s=170667a&w=0&k=20&c=UjmGkC6PnxehHcVbKTkU7-qwchInFV4Cwt328VsYPa4=",
+      img5:"https://media.istockphoto.com/id/895081824/photo/tourist-visiting-spain.webp?b=1&s=170667a&w=0&k=20&c=ptIdYDtsmEhWJ7aVg-88Egu93-jmSYh3QfxobCR_Mjo="
+    }
     
     return <Box w="100%" textAlign="left" p="30px">
         <Accordion display="flex" w="100%"  justifyContent="space-evenly" h="500px">
@@ -30,7 +37,7 @@ export const Accordian=({img})=>{
             <ListItem><Text color="black" fontSize="xl">All our provisions follow the strictest standards of hygiene.</Text></ListItem>
         </UnorderedList>
     
-    </AccordionPanel>:<Image src={img} w="100%" h="300px" mt="20px" borderRadius="20px"></Image>}
+    </AccordionPanel>:<Image src={images.img1} w="100%" h="300px" mt="20px" borderRadius="20px"></Image>}
     
   </AccordionItem>
 
@@ -49,7 +56,7 @@ export const Accordian=({img})=>{
             <ListItem><Text color="black" fontSize="xl">We will provide you with adequate camping gear like waterproof tents and sleeping bags</Text></ListItem>
             <ListItem><Text color="black" fontSize="xl">All our provisions follow the strictest standards of hygiene.</Text></ListItem>
         </UnorderedList>
-        </AccordionPanel>:<Image src={img} w="100%" h="300px" mt="20px" borderRadius="20px"></Image>
+        </AccordionPanel>:<Image src={images.img2} w="100%" h="300px" mt="20px" borderRadius="20px"></Image>
     }
   </AccordionItem>
 
@@ -68,7 +75,7 @@ export const Accordian=({img})=>{
             <ListItem><Text color="black" fontSize="xl">We will provide you with adequate camping gear like waterproof tents and sleeping bags</Text></ListItem>
             <ListItem><Text color="black" fontSize="xl">All our provisions follow the strictest standards of hygiene.</Text></ListItem>
         </UnorderedList>
-        </AccordionPanel>:<Image src={img} w="100%" h="300px" mt="20px" borderRadius="20px"></Image>
+        </AccordionPanel>:<Image src={images.img3} w="100%" h="300px" mt="20px" borderRadius="20px"></Image>
     }
   </AccordionItem>
 
@@ -88,7 +95,7 @@ export const Accordian=({img})=>{
             <ListItem><Text color="black" fontSize="xl">We will provide you with adequate camping gear like waterproof tents and sleeping bags</Text></ListItem>
             <ListItem><Text color="black" fontSize="xl">All our provisions follow the strictest standards of hygiene.</Text></ListItem>
         </UnorderedList>
-        </AccordionPanel>:<Image src={img} w="100%" h="300px" mt="20px" borderRadius="20px"></Image>
+        </AccordionPanel>:<Image src={images.img4} w="100%" h="300px" mt="20px" borderRadius="20px"></Image>
     }
   </AccordionItem>
   <AccordionItem w="33%" p="15px">
@@ -106,7 +113,7 @@ export const Accordian=({img})=>{
             <ListItem><Text color="black" fontSize="xl">We will provide you with adequate camping gear like waterproof tents and sleeping bags</Text></ListItem>
             <ListItem><Text color="black" fontSize="xl">All our provisions follow the strictest standards of hygiene.</Text></ListItem>
         </UnorderedList>
-        </AccordionPanel>:<Image src={img} w="100%" h="300px" mt="20px" borderRadius="20px"></Image>
+        </AccordionPanel>:<Image src={images.img5} w="100%" h="300px" mt="20px" borderRadius="20px"></Image>
     }
   </AccordionItem>
 </Accordion>
